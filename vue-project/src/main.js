88859193606d128ee9edda961ec18c710bafe032
TRAD3R <6292676@gmail.com>
2018-09-06 13:26:30 +0300

    25) Глобальные и локальные компоненты

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 /*
  * Vue.component (компонент) - переиспользуемый экземпляр vue
  * data должен быть в виде функции
+ *
+ * Это глобальный компонент
  */
 Vue.component('my_counter', {
     template: `
@@ -26,4 +28,35 @@ Vue.component('my_counter', {
 new Vue({
     el: '#app',
   // render: h => h(App)
+});
+
+/*
+ * Локальный компонент
+ * имя компонента (ключ) записывается либо в кавычках с "-" между словами
+ * либо в "верблюжьем" стиле (Vue сам изменит на правильную HTML нотацию)
+ */
+var component = {
+    template: `
+        <div>
+            <h2>Счетчик: {{counter}}</h2>
+            <button @click="counter++">+1</button>
+        </div>
+        `,
+    data: function() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        add: function () {
+            this.counter++;
+        }
+    }
+};
+
+new Vue({
+    el: '#app2',
+    components: {
+        myCounter: component
+    }
 });
