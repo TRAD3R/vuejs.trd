d7d86c61c4699f0bd47cab11c2a37ae0f3ec8d75
TRAD3R <6292676@gmail.com>
2018-09-06 11:43:39 +0300

    23) Введение в компоненты (vue.component)

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,7 +1,29 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
 
+/*
+ * Vue.component (компонент) - переиспользуемый экземпляр vue
+ * data должен быть в виде функции
+ */
+Vue.component('my_counter', {
+    template: `
+        <div>
+            <h2>Счетчик: {{counter}}</h2>
+            <button @click="counter++">+1</button>
+        </div>
+        `,
+    data: function() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        add: function () {
+            this.counter++;
+        }
+    }
+});
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    el: '#app',
+  // render: h => h(App)
+});
